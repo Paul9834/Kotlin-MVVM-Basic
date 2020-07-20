3c2c3fe00a52709b266d209570311a87cbe8001c
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-07-20 13:52:11 -0500

    Initial commit

---
+++ app/src/main/java/com/paul9834/learn_kotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.paul9834.learn_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+}
